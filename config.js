4f0132bac518601c519e4acffe7ddce711869632
Ongeri-2 <ongeriongeri13145618@gmail.com>
2024-12-10 22:10:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "rIhAWY4T#pjV-FsSCFB12JxGjgnuHoMCK_DgmCj6v9VCTjDHNDj0",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
